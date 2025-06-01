7ff7dcd29caff7ea16b2d9ddf96fe7ab7d696e44
Benfranklin m s <bfranklinms@gmail.com>
2025-06-02 00:05:36 +0530

    feat: add react-icons dependency and enhance Navbar with icon and styling

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { FaCube } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
     <div>
-        <nav>
-            <div>
-                <span>Whispers</span>
+        <nav className='flex justify-between bg-pink-700 text-white p-6'>
+            <div className='flex items-center gap-2'>
+                <FaCube className='text-2xl'/>
+                <span className='font-bold text-2xl'>Whispers</span>
             </div>
-            <ul>
+            <ul className='flex gap-4'>
                 <li>Home</li>
                 <li>Confessions</li>
                 <li>Add Confession</li>
             </ul>
+            <div>
+                
+            </div>
         </nav>
     </div>
   )
