0871ed81883ca4ecdd12f74828ebbebf8ed11b95
Benfranklin m s <bfranklinms@gmail.com>
2025-06-02 19:51:15 +0530

    updated hero section

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex justify-center items-center bg-pink-700 text-white p-7 relative bottom-0 w-full'>
+      <span>@2025Whispers.All Rights Reserved</span>
+    </div>
   )
 }
 
--- Client/src/components/Hero.jsx
+++ Client/src/components/Hero.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+   <section className="w-full min-h-screen bg-white px-2 pt-10 md:pt-0 flex justify-center items-start md:items-center">
+      <div className="flex justify-center items-center relative w-full max-w-4xl h-[70vh] rounded-xl overflow-hidden border border-white/20 bg-white/10 backdrop-blur-md shadow-lg">
+        {/* Blurred Background Image Inside Box */}
+        <img
+          src="/HeroSection.jpg"
+          alt="Background"
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-black/40 backdrop-blur-md" />
+
+        {/* Content */}
+        <div className="relative z-10 text-center px-6 py-16">
+          <h1 className="text-white text-3xl md:text-5xl font-extrabold">
+            Share Your Secrets, <br /> Anonymously
+          </h1>
+          <p className="text-gray-200 mt-4 text-base md:text-lg">
+            Unburden your heart and mind. Share your deepest secrets and
+            confessions with the world, knowing your identity remains protected.
+          </p>
+          <button className="mt-6 px-6 py-3 bg-pink-600 hover:bg-red-700 text-white font-semibold rounded-full transition">
+            Confess Now
+          </button>
+        </div>
+      </div>
+    </section>
   )
 }
 
