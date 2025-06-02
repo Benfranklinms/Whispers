2c168c27fe90499a5e581d9d2a226bf710da3811
Benfranklin m s <bfranklinms@gmail.com>
2025-06-02 11:04:58 +0530

    protected sensitive information

--- Server/server.js
+++ Server/server.js
@@ -9,8 +9,10 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Hello, world!");
-})
+});
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log("Server is running on port 3000");
-})
+});
