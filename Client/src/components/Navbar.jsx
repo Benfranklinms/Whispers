2fd7b453a63ca5cc56dace1c3fd1d31ca866b8bf
Benfranklin m s <bfranklinms@gmail.com>
2025-06-04 14:42:31 +0530

    some changes

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -54,6 +54,7 @@ const Navbar = () => {
                         <Link to = '/' className='hover:bg-red-900 block w-full hover:rounded px-2 py-1'>Home</Link>
                         <Link to = '/confessions' className='hover:bg-red-900 block w-full hover:rounded px-2 py-1'>Confessions</Link>
                         <Link to = '/add-confession' className='hover:bg-red-900 block w-full hover:rounded px-2 py-1'>Add Confession</Link>
+                        <Link to = 'https://github.com/Benfranklinms/Whispers' className='hover:bg-red-900 block w-full hover:rounded px-2 py-1'>Source code</Link>
                         <button className='text-left hover:bg-red-900 block w-full hover:rounded px-2 py-1'>Logout</button>
                     </div>
                 )
